2bfc733739fcb22d93a70978e2f310ff77a6584b
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-10-28 21:10:43 +0300

    update

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurant.screen';
 import {  ThemeProvider } from 'styled-components/native';
-import {theme} from './src/infrastructure/theme'
+import {theme} from './src/infrastructure/theme';
+import { useFonts as useOswald,Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts as useLato,Lato_400Regular } from "@expo-google-fonts/oswald";
+
 export default function App() {
+  const [oswalLoaded] =useOswald({
+    Oswald_400Regular
+  });
+  const [latoLoaded] =useLato({
+    Oswald_400Regular
+  });
+
+  if(!oswalLoaded || !latoLoaded)
+    {
+      return null;
+    }
   return (
   <>
   <ThemeProvider theme={theme}>
--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,7 +1,9 @@
 import React  from "react";
 import {   StyleSheet,   } from 'react-native';
 import styled from "styled-components/native"
-import { Card, Text } from 'react-native-paper';
+import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+import star from "../../../../assets/star";
 
 
 const RestaurantCard= styled(Card)`
@@ -11,11 +13,24 @@ const RestaurantCardCover= styled(Card.Cover)`
 color: ${(props)=>props.theme.colors.ui.primary};
 padding:${(props)=>props.theme.space[3]};
 `;
-
+const Address =styled.Text`
+font-family:${(props)=>props.theme.fonts.body};
+font-size:${(props)=>props.theme.fontSizes.caption};
+`;
 
 const Title =styled.Text`
+font-family:${(props)=>props.theme.fonts.body};
+font-size:${(props)=>props.theme.fontSizes.body};
+color: ${(props)=>props.theme.colors.ui.primary};
+`;
+
+const Info =styled.View`
 padding:${(props)=>props.theme.space[3]};
-color: ${(props)=>props.theme.colors.ui.primary}
+`;
+
+const Rating =styled.View`
+flex-direction:row;
+padding:${(props)=>props.theme.space[2]};
 `;
 
 export const RestaurantInfo = ({restaurant ={}}) =>{
@@ -23,15 +38,27 @@ export const RestaurantInfo = ({restaurant ={}}) =>{
 name= 'Pizza Slice',
 icon,
 photos =["https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?b=1&s=612x612&w=0&k=20&c=ZcLXrogjpyc5froC5ZIP-0uepbhldATwmCbt3mzViGQ="],
-address="1000",
+address="1000 some random street",
 isOpenNow=true,
 rating= 4,
 isClosedTemporarily
-    } = restaurant
+    } = restaurant;
+
+    const ratingArray= Array.from(new Array(Math.floor(rating)));
 return(
     <RestaurantCard elevation={5} >
         < RestaurantCardCover key={name}  source={{uri:photos[0]}}/>
+        <Info>
         <Title>{name}</Title>
+        <Rating>
+        {ratingArray.map((_, index) => (
+    <SvgXml key={index} xml={star} width={20} height={20} />
+))}  
+       </Rating>
+       
+        <Address>{address}</Address>
+        </Info>
+     
     </RestaurantCard>
 )
 
