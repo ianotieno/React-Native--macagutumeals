18c0182f9593f1339b9aaf6131f65f714e568d9f
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-11-02 14:51:06 +0300

    update

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -1,38 +1,36 @@
-import React,{useContext,useEffect, useState} from "react";
-import { Searchbar } from 'react-native-paper';
-import styled from "styled-components/native"
+import React, { useContext, useState, useEffect } from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
 import { LocationContext } from "../../../services/location/location.context";
 
-const SearchContainer=styled.View`
-padding:${(props)=>props.theme.space[3]};
-positin:absolute;
-z-index:999;
-top:30px;
-width:100%;
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
 
-export const Search =() =>{
-   
-    const {keyword, search} =useContext(LocationContext);
-    const [searchKeyword,setSearchKeyword]= useState(keyword);
-    useEffect(()=>{
-        search(keyword)
-    },[keyword])
-   return (
+export const Search = () => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
+  return (
     <SearchContainer>
-    <Searchbar 
-    placeholder='search'
-     value={searchKeyword}
-     onSubmitEditing={()=>{
-        search(searchKeyword)
-     }}
-     onChangeText={(text)=>{
-        if(!text.length){
-          
-        }
-        setSearchKeyword(text)
-     }}
-    />
-      </SearchContainer>
-      )
+      <Searchbar
+        placeholder="Search for a location"
+        icon="map"
+        value={searchKeyword}
+        onSubmitEditing={() => {
+          search(searchKeyword);
+        }}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
+  );
 };
--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,34 +1,33 @@
-import React,{useContext,useEffect, useState} from "react";
-import { Searchbar } from 'react-native-paper';
-import styled from "styled-components/native"
+import React, { useContext, useState, useEffect } from "react";
+import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
 import { LocationContext } from "../../../services/location/location.context";
 
-const SearchContainer=styled.View`
-padding:${(props)=>props.theme.space[3]};
-
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `;
 
-export const Search =() =>{
-   
-    const {keyword, search} =useContext(LocationContext);
-    const [searchKeyword,setSearchKeyword]= useState(keyword);
-    useEffect(()=>{
-        search(searchKeyword)
-    },[])
-   return (
+export const Search = ({ isFavouritesToggled, onFavouritesToggle }) => {
+  const { keyword, search } = useContext(LocationContext);
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
+  return (
     <SearchContainer>
-    <Searchbar placeholder='search'
-     value={searchKeyword}
-     onSubmitEditing={()=>{
-        search(searchKeyword)
-     }}
-     onChangeText={(text)=>{
-        if(!text.length){
-          
-        }
-        setSearchKeyword(text)
-     }}
-    />
-      </SearchContainer>
-      )
+      <Searchbar
+        icon={isFavouritesToggled ? "heart" : "heart-outline"}
+        onIconPress={onFavouritesToggle}
+        placeholder="Search for a location"
+        value={searchKeyword}
+        onSubmitEditing={() => {
+          search(searchKeyword);
+        }}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
+  );
 };
