9780cb5ea254197ee05462e7b7f5059ac9e7613e
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-11-03 12:57:17 +0300

    update

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,21 +1,48 @@
-import React, { useContext } from "react";
-import MapView from "react-native-maps";
+import React, { useContext, useEffect, useState } from "react";
+import MapView , { Marker }  from "react-native-maps";
 import styled from "styled-components/native";
-import { Search } from '../components/search.component'
+import { Search } from "../components/search.component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurant.context";
 
-
-
-const Map =styled(MapView)`
-flex: 1;
+const Map = styled(MapView)`
+  flex: 1;
 `;
 export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
 
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    const latDelta = northeastLat - southwestLat;
+    setLatDelta(latDelta);
+  }, [location, viewport]);
 
   return (
     <>
-    <Search/>
-    <Map /> 
+      <Search />
+      <Map region={{
+           latitude:lat,
+           longitude: lng,
+           latitudeDelta : latDelta,
+           longitudeDelta: 0.02,
+
+      }}>
+        {restaurants.map((restaurant) => {
+          return (
+          <Marker
+          key={restaurant.name}
+          title ={restaurant.name}
+          coordinate={{
+            latitude:restaurant.geometry.location.lat,
+            longitude: restaurant.geometry.location.lng,
+          }}
+         />);
+        })}
+      </Map>
     </>
-    
   );
 };
